6677e6727ee4c819affe028bfcae610a0e270e84
Alsu Kirillova <sai-alsu@mail.ru>
2023-03-06 11:16:11 +0300

    Commite

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import {Test} from "./test/Test";
 
 
 function App() {
     return (
         <div className="App">
+            <div className="menu">
+                <Link to={"login"}>Логинизация</Link>
+                <Link to={"auth"}>Регистрация</Link>
+                <Link to={"profile"}>Профайл</Link>
+                <Link to={"login/password"}>Восстановление пароля</Link>
+                <Link to={"login/password/new"}>Ввод нового пароля</Link>
+                <Link to={"/"}>Тест</Link>
+            </div>
             <Routes>
                 <Route path={"/login"} element={<h1>Логинизация</h1>}/>
                 <Route path={"/auth"} element={<h1>Регистрация</h1>}/>
                 <Route path={"/profile"} element={<h1>Профайл</h1>}/>
-                <Route path={"*"} element={<h1>404</h1>}/>
                 <Route path={"/login/password"} element={<h1>Восстановление пароля</h1>}/>
                 <Route path={"/login/password/new"} element={<h1>Ввод нового пароля</h1>}/>
-                <Route path={"/test"} element={<Test/>}/>
+                <Route path={"/"} element={<Test/>}/>
+                <Route path={"*"} element={<h1>404</h1>}/>
             </Routes>
         </div>
     );
